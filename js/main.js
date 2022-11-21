a66435deb7a1863a5e7488287ff2cd872472c7c1
Marcelo Bilby Barbosa <suporte.oraculo@gmail.com>
2022-11-21 12:44:59 -0400

    adicionando ROBOTRON
    Adicionando projeto ROBOTRON

---
+++ js/main.js
@@ -0,0 +1,67 @@
+const controle = document.querySelectorAll("[data-controle]")
+const estatistica = document.querySelectorAll("[data-estatistica]")
+console.log(estatistica)
+const pecas = {
+  "bracos": {
+      "forca": 29,
+      "poder": 35,
+      "energia": -21,
+      "velocidade": -5
+  },
+
+  "blindagem": {
+      "forca": 41,
+      "poder": 20,
+      "energia": 0,
+      "velocidade": -20
+  },
+  "nucleos":{
+      "forca": 0,
+      "poder": 7,
+      "energia": 48,
+      "velocidade": -24
+  },
+  "pernas":{
+      "forca": 27,
+      "poder": 21,
+      "energia": -32,
+      "velocidade": 42
+  },
+  "foguetes":{
+      "forca": 0,
+      "poder": 28,
+      "energia": 0,
+      "velocidade": -2
+  }
+}
+
+/* const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar"); */
+
+//const braco = document.querySelector("#braco");
+
+
+controle.forEach ((elemento) => {
+  elemento.addEventListener("click", (evento) => {
+    manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+    atualizaEstatistica(evento.target.dataset.pecas)
+  })
+})
+
+// const robotron = document.querySelector("#robotron")
+
+function manipulaDados (operacao, controle) {
+  const peca = controle.querySelector("[data-contador]")
+  if (operacao === "-" && peca.value > 0) {
+    peca.value = parseInt(peca.value) - 1
+  } else if (operacao === "+") {
+    peca.value = parseInt(peca.value) + 1
+  }
+}
+
+function atualizaEstatistica (peca) {
+  estatistica.forEach((elemento)=> {
+    elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+  })
+  console.log(pecas[peca])
+}
